d7ccda69417bc2c82b2fa7efdb788276d6633bdf
theDragonbudd@gmail.com <theDragonbudd@gmail.com>
2020-06-11 17:38:15 +0700

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index( int id)
+        {
+            int a = 2;
+            ViewBag.a = a;
+            return View();
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Public()
+        {
+            ViewBag.Message = "Public List";
+
+            return View();
+        }
+    }
+}
